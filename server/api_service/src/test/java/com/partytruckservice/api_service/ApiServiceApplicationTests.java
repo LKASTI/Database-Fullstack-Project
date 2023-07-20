6880f149ef6276d33d53f69f3b60c191942a2413
Landin Kasti <lkastiman@gmail.com>
2023-07-20 13:01:17 -0500

    initial commit

---
+++ server/api_service/src/test/java/com/partytruckservice/api_service/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partytruckservice.api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
