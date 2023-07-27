61b6394da66041b0882ed9611058f67ede9bc125
Landin Kasti <lkastiman@gmail.com>
2023-07-27 15:14:11 -0500

    added more comments to create event page

--- client/src/pages/CreateEvent.jsx
+++ client/src/pages/CreateEvent.jsx
@@ -10,6 +10,7 @@ const CreateEvent = () => {
     const [eventAddress, setEventAddress] = useState()
     const [assignedEmployee, setAssignedEmployee] = useState()
     const [validEvent, setValidEvent] = useState(false)
+    const [createdEvent, setCreatedEvent] = useState(false)
 
     //getting the cID from the URL
     const params = useParams()
@@ -18,7 +19,11 @@ const CreateEvent = () => {
 
     const checkTimes = async () => {
         //check if the given times conflict with a prexisting event and if an employee is able to attend
-        //store the employee id in assignedEmployee
+        //if the times are valid
+            //store the employee id in assignedEmployee
+            //if all event fields are filled 
+                //set validEvent state to TRUE which allows submit button to become clickable and highlighted green
+        
     }
 
     const createAnEvent = async () => {
@@ -29,6 +34,9 @@ const CreateEvent = () => {
           event address, start and end times,
           and any available vehicle ID
         */
+
+        //set createdEvent state to TRUE
+        //allows next button to be clickable and highlighted green
     }
 
     return(
