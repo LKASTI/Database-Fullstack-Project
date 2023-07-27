27e6e87b7b08e674c3e78d0d4fdcc5f7660bf221
Landin Kasti <lkastiman@gmail.com>
2023-07-27 16:25:19 -0500

    added more comments describing create custom package page validation

--- client/src/pages/CreateCustomPackage.jsx
+++ client/src/pages/CreateCustomPackage.jsx
@@ -6,15 +6,31 @@ const CreateCustomPackage = () => {
     const [availableItems, setAvailableItems] = useState()
     const [newCustomPackage, setNewCustomPackage] = useState()
     const [selectedItem, setSelectedItem] = useState()
+
     const [newItemName, setNewItemName] = useState() 
-    const [newItemQuantity, setNewItemQuantity] = useState() 
+    const [newItemQuantity, setNewItemQuantity] = useState()
+
+    const [validItemSelection, setValidItemSelection] = useState(false)
 
     useEffect(() => {
         //get all items from db and store in availableItems state
     }, [])
 
+    const checkItemQuantity = (e) => {
+        //selectedItem cannot be null
+        const quantity = e.target.value
+
+        if(quantity > selectedItem.stock) 
+            alert("Please enter a quantity less than " + selectedItem.stock)
+        else
+        {
+            setNewItemQuantity(quantity)
+        }
+    }
+
     return(
-        <div className="CreateCustomPackage">
+        <div className="createCustomPackage">
+            
         </div>
     )
 }
