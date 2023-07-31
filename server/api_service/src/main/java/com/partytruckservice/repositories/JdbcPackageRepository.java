da32ec0cb894b44be85835fa043e8e95c6e3a502
sanger96 <gauravkumarsanger@gmail.com>
2023-07-30 20:14:49 -0500

    added 3 files (untested)

---
+++ server/api_service/src/main/java/com/partytruckservice/repositories/JdbcPackageRepository.java
@@ -0,0 +1,58 @@
+package com.partytruckservice.repositories;
+
+import com.partytruckservice.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class JdbcPackageRepository implements PackageRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    // add item to package
+    @Override
+    public int addItemToPackage(int itemID, PACKAGE pack, int quantity) {
+        return jdbcTemplate.update(
+                "INSERT INTO itemIncludesPackage (iID, pID, quantity) VALUES(?,?,?)",
+                itemID, pack.getPackageID(), quantity);
+    }
+
+    // remove item from package
+    @Override
+    public int removeItemFromPackage(int itemID, int packageID) {
+        return jdbcTemplate.update(
+                "DELETE itemIncludesPackage WHERE iID=? AND pID=?",
+                itemID, packageID);
+    }
+
+    @Override
+    public int save(PACKAGE pack) {
+        return jdbcTemplate.update(
+                "INSERT INTO PACKAGE (packageID, name, discount) VALUES(?,?,?)",
+                pack.getPackageID(), pack.getName(), pack.getDiscount());
+    }
+
+    @Override
+    public int deletePackage(PACKAGE pack) {
+        return jdbcTemplate.update(
+                "DELETE PACKAGE WHERE packageID=?", pack.getPackageID());
+    }
+
+    @Override
+    public int getAllItemsInPackageList(PACKAGE pack) {
+        return jdbcTemplate.update(
+                "SELECT ITEM.name FROM ITEM, itemIncludesPackage WHERE itemIncludesPackage.iID=ITEM.itemID AND itemIncludesPackage.pID=?",
+                pack.getPackageID());
+    }
+
+    @Override
+    public int getAllPresetPackages(PACKAGE pack) {
+        return jdbcTemplate.update(
+                "SELECT PACKAGE.name FROM PACKAGE WHERE PACKAGE.packageID=?",
+                pack.getPackageID());
+    }
+}
